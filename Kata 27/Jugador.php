0ae5e0bdfc733ba52f4634a8f1b230376285b672
Agonpas <agonpas@gmail.com>
2024-02-07 20:41:07 +0100

    Función verMateriales y verCompras añadidas

--- Kata 27/Jugador.php
+++ Kata 27/Jugador.php
@@ -9,32 +9,48 @@ class Jugador{
         $this->materiales = $materiales; // array para los materiales y las cantidades
         $this->compras = []; // array para almacenar las compras
     }
-    private function verificarRecursos(array $costo): bool {
-        foreach ($costo as $recurso => $cantidad) {
-            if (!isset($this->materiales[$recurso]) || $this->materiales[$recurso] < $cantidad) {
-                return false;
+    /*Función para verificar si tenemos materiales para hacer la compra*/
+    private function verificarMateriales(array $costo): bool { // la función recibe el array $costo (precio de la compra)
+        foreach ($costo as $material => $cantidad) { //recorremos array para ver el material y la cantidad requerida
+            if (!isset($this->materiales[$material]) || $this->materiales[$material] < $cantidad) { // en el caso de que el material no exista o la cantidad sea menor que la requerida 
+                return false; // devolvemos false
             }
         }
-        return true;
+        return true; // si ninguna de las 2 condiciones se han cumplido podemos efectuar la compra 
     }
-    private function realizarCompra(array $costo, string $tipoCompra): void {
-        foreach ($costo as $recurso => $cantidad) {
-            $this->materiales[$recurso] -= $cantidad;
+    /*Función para descontar los materiales una vez efectuada la compra*/
+    private function realizarCompra(array $costo, string $tipoCompra): void { // la función recibe el array $costo (precio de la compra) y el nombre del elemento comprado
+        foreach ($costo as $material => $cantidad) { // recorremos el array de costos para extraer el material y la cantidad
+            $this->materiales[$material] -= $cantidad; // restamos el material 
         }
-
-        if (isset($this->compras[$tipoCompra])) {
-            $this->compras[$tipoCompra]++;
+        /* ahora añadimos el elemento comprado al array de compras*/
+        if (isset($this->compras[$tipoCompra])) { //verificamos si el tipo de compra existe
+            $this->compras[$tipoCompra]++; // si existe añadimos 1 al tipo de compra en el array compras
         } else {
-            $this->compras[$tipoCompra] = 1;
+            $this->compras[$tipoCompra] = 1; // en el caso de no existir la compra la creamos la añadimos el array y le damos la cantidad de 1
         }
     }
+    /* funcion para comprar camino*/
     public function comprarCamino() {
-        $costo = ['madera' => 1, 'arcilla' => 1];
-        if ($this->verificarRecursos($costo)) {
-            $this->realizarCompra($costo, 'camino');
-            echo "Se ha realizado la compra correctamente" . PHP_EOL;
+        $costo = ['madera' => 1, 'arcilla' => 1]; // creamos array costo con los materiales y la cantidad necesario para la compra de un camino
+        if ($this->verificarMateriales($costo)) { // usamos la funciónn para ver si tenemos los materiales necesarios
+            $this->realizarCompra($costo, 'camino'); // en el caso de cumplir la condición realizamos la compra
+            echo "Se ha realizado la compra correctamente" . PHP_EOL; // comunicamos que la compra se ha realizado
         } else {
-            echo "No tienes materiales suficentes para un camino" . PHP_EOL;
+            echo "No tienes materiales suficentes para un camino" . PHP_EOL; // informamos en el caso de no tener los materiales para realizar la compra
+        }
+    }
+    /*creamos una función para ver los materiales que disponemos*/
+    public function verMateriales() {
+        echo "Materiales de $this->nombre:" . PHP_EOL;
+        foreach ($this->materiales as $material => $cantidad) {
+            echo "$material: $cantidad" . PHP_EOL;
+        }
+    }
+    public function verCompras() {
+        echo "Las compras de $this->nombre:" . PHP_EOL;
+        foreach ($this->compras as $compra => $cantidad) {
+            echo "$compra: $cantidad" . PHP_EOL;
         }
     }
 }
--- Kata 27/index.php
+++ Kata 27/index.php
@@ -1,6 +1,10 @@
 <?php
 include_once 'Jugador.php';
 
-$jugador1 = new Jugador ("Alberto", ['madera' => 3, 'lana' => 2, 'arcilla' => 0, 'mineral' => 1, 'cereal' => 2]);
+$jugador1 = new Jugador ("Alberto", ['madera' => 3, 'lana' => 2, 'arcilla' => 2, 'mineral' => 1, 'cereal' => 2]);
+$jugador1->verMateriales();
 $jugador1->comprarCamino();
+$jugador1->verMateriales();
+$jugador1->verCompras();
+
 ?>
