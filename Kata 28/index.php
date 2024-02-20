1f3a7a9d0bbe65f2059105db473dc4a14a74a59e
Agonpas <agonpas@gmail.com>
2024-02-20 19:39:28 +0100

    Ahora funciona con numeros de varias cifras

--- Kata 28/index.php
+++ Kata 28/index.php
@@ -3,29 +3,43 @@ echo "Introduce una cifra" . PHP_EOL;
 $numero = fgets(STDIN);
 echo "Introduce una unidad" . PHP_EOL;
 $unidad = fgets(STDIN);
-$numeros = crearArray($numero, $unidad);
+$numeros = crearArray($numero);
+$unidades = crearArrayUnidad($unidad);
 
-function crearArray(int $numero, int $unidad) {
+function crearArray(int $numero) {
 
     $numeros = array();
     while($numero!= 0){ 
         $numeros[] = $numero % 10;
         $numero = intval($numero/10); // intval convierte el resultado en entero
         }
-    return $numeros;
+    return $numeros;  
+}
+function crearArrayUnidad(int $unidad){
+    $unidades = array();
+    while($unidad!= 0){ 
+        $unidades[] = $unidad % 10;
+        $unidad = intval($unidad/10); // intval convierte el resultado en entero
+        }
+    return $unidades;
+}
+function buscarCifra(array $numeros, array $unidades, int $numero, int $unidad) {
+
+    
+
     
 }
-function buscarCifra(array $numeros,int $numero, int $unidad) {
-    
-    $contiene = array_search($unidad, $numeros); 
-     if ($contiene !== false){
-         return "La cifra " . $unidad . " está dentro de: " . $numero;
-      } else {
-          return "La cifra $unidad no está dentro de $numero";
-     }
+foreach ($unidades as $uni) {
+    $contiene = array_search($uni, $numeros); 
+ if ($contiene !== false){
+     echo "La cifra " . $uni . " está dentro de: " . $numero . PHP_EOL;
+  } else {
+      echo "La cifra $uni no está dentro de $numero" . PHP_EOL;
+ }
+
 }
 
-$resultado = buscarCifra($numeros, $numero, $unidad);
-echo $resultado;
+/*$resultado = buscarCifra($numeros, $unidades, $numero, $unidad);
+echo $resultado;*/
 
 ?>
