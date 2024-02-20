4fd68a9791a05648b811a11547b0bed917beb3dd
Agonpas <agonpas@gmail.com>
2024-02-20 19:07:57 +0100

    dividido en 2 funciones

--- Kata 28/index.php
+++ Kata 28/index.php
@@ -3,33 +3,29 @@ echo "Introduce una cifra" . PHP_EOL;
 $numero = fgets(STDIN);
 echo "Introduce una unidad" . PHP_EOL;
 $unidad = fgets(STDIN);
+$numeros = crearArray($numero, $unidad);
 
+function crearArray(int $numero, int $unidad) {
 
-function buscarCifra(int $numero, int $unidad) {
     $numeros = array();
-    $cifra = $numero;
     while($numero!= 0){ 
         $numeros[] = $numero % 10;
         $numero = intval($numero/10); // intval convierte el resultado en entero
         }
-        $contiene = array_search($unidad, $numeros); 
-        if ($contiene !== false){
-            return "La cifra " . $unidad . " está dentro de: " . $cifra;
-        } else {
-            return "La cifra $unidad no está dentro de $cifra";
-        }
+    return $numeros;
+    
+}
+function buscarCifra(array $numeros,int $numero, int $unidad) {
+    
+    $contiene = array_search($unidad, $numeros); 
+     if ($contiene !== false){
+         return "La cifra " . $unidad . " está dentro de: " . $numero;
+      } else {
+          return "La cifra $unidad no está dentro de $numero";
+     }
 }
 
-$resultado = buscarCifra($numero, $unidad);
+$resultado = buscarCifra($numeros, $numero, $unidad);
 echo $resultado;
 
-
-
-
-
-/*if ($contiene2 !== false){
-    echo "La cifra $unidad2 está dentro de: " . $numero;
-} else {
-    echo "La cifra $unidad2 no está dentro de $numero";
-}*/
 ?>
